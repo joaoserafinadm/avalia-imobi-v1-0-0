1c572c22afc7ca16ccb34aea5b1cdf39833dc628
joaoserafinadm <joaoserafin.adm@gmail.com>
2023-09-14 21:45:17 -0300

    feat: navbar redux states & font montserrat

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -3,13 +3,13 @@ export default function baseUrl() {
     // Desenvolvimento
     // const baseUrl = 'http://localhost:3000'
 
-    const baseUrl = 'https://avalia-imobi-v1-0-0-as4fdrwam-joaoserafinadm.vercel.app/'
+    // const baseUrl = 'https://avalia-imobi-v1-0-0-as4fdrwam-joaoserafinadm.vercel.app/'
 
     // Teste
     // const baseUrl = 'https://avalia-imobi-v1-0-0.vercel.app'
 
     //Produção
-    // const baseUrl = 'https://app.avaliaimobi.com.br'
+    const baseUrl = 'https://app.avaliaimobi.com.br'
 
     return baseUrl
 }
