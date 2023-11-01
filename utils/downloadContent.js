4c05ea6fca468b921ea34811a267ccaf30bf478e
joaoserafinadm <joaoserafin.adm@gmail.com>
2023-10-31 23:29:22 -0300

    feat: fixed topics bottom

--- utils/downloadContent.js
+++ utils/downloadContent.js
@@ -3,8 +3,18 @@ import html2canvas from "html2canvas";
 import saveAs from "./saveAs";
 
 export default function downloadContent(id, fileName) {
-    
-    html2canvas(document.querySelector(`#${id}`)).then(canvas => {
+
+    const chartContainer = document.getElementById(id);
+  const scale = 10;
+
+    html2canvas(chartContainer, {
+        scale,
+        letterRendering: 1,
+        allowTaint: true,
+        useCORS:true,
+        onrendered: function (canvas) {saveAs(canvas.toDataURL(), fileName) }
+    })
+    .then(canvas => {
         saveAs(canvas.toDataURL(), fileName)
     });
 }
